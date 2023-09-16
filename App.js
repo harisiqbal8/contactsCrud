0df54de6f9245f7fe9b831bd371c8ed9d653dce2
harisiqbal8 <harisiqbaldec@gmail.com>
2023-09-16 17:35:21 +0500

    3rd Commit

--- App.js
+++ App.js
@@ -1,34 +1,43 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import Contacts from 'react-native-contacts';
+import * as Contacts from 'expo-contacts';
 
 function AddContactScreen() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
-  const handleAddContact = () => {
-    const newContact = {
-      givenName: firstName,
-      familyName: lastName,
-      phoneNumbers: [{
-        label: 'mobile',
-        number: phoneNumber,
-      }],
-    };
+  const handleAddContact = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
 
-    Contacts.addContact(newContact)
-      .then(contact => {
-        console.log('Contact added successfully:', contact);
+    if (status === 'granted') {
+      const contact = {
+        [Contacts.Fields.FirstName]: firstName,
+        [Contacts.Fields.LastName]: lastName,
+        [Contacts.Fields.PhoneNumbers]: [
+          {
+            label: 'mobile',
+            number: phoneNumber,
+          },
+        ],
+      };
+
+      try {
+        await Contacts.addContactAsync(contact);
+        console.log('Contact added successfully!');
         // You can navigate to another screen or perform further actions here
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Error adding contact:', error);
-      });
+      }
+    } else {
+      console.log('Permission to write contacts denied.');
+      // Handle the case where permission is denied
+    }
   };
 
   return (
     <View style={styles.container}>
+      {/* User interface elements for input */}
       <Text style={styles.label}>First Name:</Text>
       <TextInput
         style={styles.input}
@@ -59,7 +68,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    marginTop: 50,
+    marginTop: 35,
   },
   label: {
     fontSize: 16,
